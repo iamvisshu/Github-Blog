ccc83ca0a9d0c00e892908a4a9b859e4082d2ad2
Vishal Vishwakarma <vishwakarmavishal14@gmail.com>
2025-10-04 23:04:00 +0530

    updated footer

--- components/Footer.js
+++ components/Footer.js
@@ -1,7 +1,7 @@
 export default function Footer() {
   return (
     <footer style={{ marginTop: "2rem", padding: "1rem", borderTop: "1px solid #eee", textAlign: "center" }}>
-      © {new Date().getFullYear()} Github-Blog by @iamvishu.
+      © {new Date().getFullYear()} Created With ❤️ By <a href="https://github.com/iamvisshu" target="_blank">@iamvisshu</a>
     </footer>
   );
 }
