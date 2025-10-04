4dcd75accebe787f290089f309f8ee5aa1c79dbc
Vishal Vishwakarma <vishwakarmavishal14@gmail.com>
2025-10-04 22:31:08 +0530

    Added favicon
    Added favicon

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,15 @@
 import { ThemeProvider } from "next-themes";
 import "../styles/globals.css";
 import Layout from "../components/Layout";
+import Head from "next/head";
 
 export default function MyApp({ Component, pageProps }) {
   return (
     <ThemeProvider attribute="class">
+      <Head>
+        <link rel="icon" href="images/favicon.ico" />
+        <title>Vishal's Blog</title>
+      </Head>
       <Layout>
         <Component {...pageProps} />
       </Layout>
