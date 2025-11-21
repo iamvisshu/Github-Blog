e5b928eaeca0efcfab8834a32a7b941c3c78fc64
Vishal Vishwakarma <vishwakarmavishal14@gmail.com>
2025-11-21 23:48:46 +0530

    moved favicon from public/images/favicon.ico to public/favicon.ico
    moved favicon from public/images/favicon.ico to public/favicon.ico

--- pages/_document.js
+++ pages/_document.js
@@ -5,8 +5,8 @@ class MyDocument extends Document {
     return (
       <Html lang="en">
         <Head>
-          <link rel="icon" href="/images/favicon.ico" />
-          <link rel="apple-touch-icon" sizes="180x180" href="/images/favicon.ico" />
+          <link rel="icon" href="/favicon.ico" />
+          <link rel="apple-touch-icon" sizes="180x180" href="/favicon.ico" />
           {/* Optional manifest (if you have one) */}
           {/* Basic meta that should be present on initial HTML */}
           <meta name="theme-color" content="#ffffff" />
